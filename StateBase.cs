02fe2c3a6d6de0fe74ac2e122c4d8eb0e8a5ede7
Asger Strandby <asger60@gmail.com>
2023-03-25 11:25:51 +0100

    added state machine

---
+++ StateBase.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using UnityEngine;
+
+namespace Farmand.StateMachine
+{
+    public abstract class StateBase : MonoBehaviour
+    {
+        private StateMachineBase _stateMachineBase;
+
+        public float StateTime => _stateMachineBase.StateTime;
+        public void Init(StateMachineBase stateMachineBase)
+        {
+            _stateMachineBase = stateMachineBase;
+        }
+        public void OnAdd()
+        {
+            Start();
+        }
+
+        public virtual void Start()
+        {
+        
+        }
+
+        public abstract void OnExitState();
+        public abstract void OnEnterState();
+        public abstract void OnStateUpdate(float stateTime);
+    }
+}
---
+++ StateMachineBase.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Farmand.StateMachine
+{
+    public class StateMachineBase : MonoBehaviour
+    {
+        readonly List<StateBase> _states = new List<StateBase>();
+        private StateBase _currentState;
+        private StateBase _prevState;
+        public StateBase PreviousState => _prevState;
+        public float StateTime => _stateTime;
+        private float _stateTime;
+        public Action<StateBase> onStateEnter;
+
+        public virtual void Awake()
+        {
+            foreach (var stateBase in GetComponentsInChildren<StateBase>(true))
+            {
+                AddState(stateBase);
+                stateBase.Init(this);
+                stateBase.gameObject.SetActive(false);
+            }
+        }
+
+        private void AddState(StateBase state)
+        {
+            _states.Add(state);
+        }
+
+        public void ChangeState<T>() where T : StateBase
+        {
+            foreach (var state in _states)
+            {
+                if (state is T)
+                {
+                    ChangeState(state);
+                    break;
+                }
+            }
+        }
+
+        protected virtual void ChangeState(StateBase state)
+        {
+            if (state == _currentState) return;
+            _prevState = _currentState;
+            _currentState = state;
+            
+            if (_prevState != null)
+            {
+                _prevState.OnExitState();
+                _prevState.gameObject.SetActive(false);
+            }
+
+            _currentState.gameObject.SetActive(true);
+            _currentState.OnEnterState();
+            onStateEnter?.Invoke(_currentState);
+            _stateTime = 0;
+
+            print("#StateMachineBase#change state " + state.GetType());
+        }
+
+        public virtual void Update()
+        {
+            if (_currentState == null) return;
+            _stateTime += Time.unscaledDeltaTime;
+            _currentState.OnStateUpdate(_stateTime);
+        }
+
+
+        public StateBase GetCurrentState()
+        {
+            return _currentState;
+        }
+
+        public StateBase GetStateOfType<T>() where T : StateBase
+        {
+            return _states.OfType<T>().Select(stateBase => (stateBase as T)).FirstOrDefault();
+        }
+    }
+}
